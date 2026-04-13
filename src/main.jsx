950c9d4276bca32598c7d08fdcd683f30b2c79e0
Tebohomok <31349771+Tebohomok@users.noreply.github.com>
2026-04-13 20:45:08 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// redeploy fix
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, useLocation, Navigate } from "react-router-dom";
 import App from './App.jsx'
